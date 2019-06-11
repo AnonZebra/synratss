049e5aa842a14dea04c2d08fca90adc3b9810bd6
AnonZebra <bestevater@posteo.de>
2019-06-11 14:20:21 +0200

    added output for no. of valid items by letters/digits categories, and for corresponding proportions of black/hazy/blue/white responses. removed extraneous "z" counter

--- R/consistency_scoring.R
+++ R/consistency_scoring.R
@@ -43,29 +43,39 @@ consistency_scoring <- function(pfp, pdir=NULL, method = "euclidean", fmt = "Luv
     }
     hexcolors[hexcolors=="#------"] <- NA #converts "no color selected" responses to NA
     hexcolors[hexcolors=="#NA"] <- NA
-    out <- array(NA, dim=c(1,47)) # "out" is a data frame for storing item consistency and mean consistency per participant values
-    z=0
+    out <- array(NA, dim=c(1,49)) # "out" is a data frame for storing item consistency and mean consistency per participant values
 
     for (i in 1:39) {
-      if ( any(is.na(hexcolors[i,2:4])) ) { #make sure non-values replaced with NA
+      if ( any(is.na(hexcolors[i,2:4])) ) { #if any of the three responses for an item is NA, the total item score is also coded as NA
         item.score <- NA
       }
       else {
         x <- hex_to_cspace(hexcolors[i,2:4], fmt)
         item.score <- dist_sum(x, method)
-        z<- z+1
         out[1,i]<- item.score
       }
     }
 
-    out[1, 40] <- mean(out[1,1:39], na.rm = TRUE) # calculates the "mean of per-item summed differences" for the participant
+    out[1, 40] <- mean(out[1,c(1:26, 30:39)], na.rm = TRUE) # calculates the "mean of per-item summed differences" for the participant (excluding umlaut letters)
     out[1, 41] <- mean(out[1,1:26], na.rm = TRUE) # for letters A-Z only, calculates the "mean of per-item summed differences" for the participant
     out[1, 42] <- mean(out[1,30:39], na.rm = TRUE) # for digits 0-9 only, calculates the "mean of per-item summed differences" for the participant
-    out[1, 43] <- z # L: this column shows for how many items there are 3 valid responses
-    out[1, 44] <- prop_col(hexcolors[, 2:4], col = "black", byrow = TRUE) #calculates proportion of items for which all responses were "black" (see prop_col's and related functions' descriptions)
-    out[1, 45] <- prop_col(hexcolors[, 2:4], col = "blue", byrow = TRUE)
-    out[1, 46] <- prop_col(hexcolors[, 2:4], col = "hazy", byrow = TRUE)
-    out[1, 47] <- prop_col(hexcolors[, 2:4], col = "white", byrow = TRUE)
+    out[1, 43] <- sum(is.na(out[1,c(1:26, 30:39)])) # calculates for how many items there are 3 valid responses, excluding umlaut letters
+    out[1, 44] <- sum(is.na(out[1,1:26])) # calculates for how many letters there are 3 valid responses, excluding umlaut letters
+    out[1, 45] <- sum(is.na(out[1,30:39])) # calculates for how many digits there are 3 valid responses
+    out[1, 46] <- prop_col(hexcolors[c(1:26, 30:39), 2:4], col = "black", byrow = TRUE) #calculates proportion of items for which all responses were "black", excluding umlaut letters
+    out[1, 47] <- prop_col(hexcolors[c(1:26, 30:39), 2:4], col = "blue", byrow = TRUE)
+    out[1, 48] <- prop_col(hexcolors[c(1:26, 30:39), 2:4], col = "hazy", byrow = TRUE)
+    out[1, 49] <- prop_col(hexcolors[c(1:26, 30:39), 2:4], col = "white", byrow = TRUE)
+    out[1, 50] <- prop_col(hexcolors[c(1:26), 2:4], col = "black", byrow = TRUE) #calculates proportion of non-umlaut letters for which all responses were "black"
+    out[1, 51] <- prop_col(hexcolors[c(1:26), 2:4], col = "blue", byrow = TRUE)
+    out[1, 52] <- prop_col(hexcolors[c(1:26), 2:4], col = "hazy", byrow = TRUE)
+    out[1, 53] <- prop_col(hexcolors[c(1:26), 2:4], col = "white", byrow = TRUE)
+    out[1, 54] <- prop_col(hexcolors[c(30:39), 2:4], col = "black", byrow = TRUE) #calculates proportion of digits for which all responses were "black"
+    out[1, 55] <- prop_col(hexcolors[c(30:39), 2:4], col = "blue", byrow = TRUE)
+    out[1, 56] <- prop_col(hexcolors[c(30:39), 2:4], col = "hazy", byrow = TRUE)
+    out[1, 57] <- prop_col(hexcolors[c(30:39), 2:4], col = "white", byrow = TRUE)
+
+
     df_total <- rbind(df_total, out)
     if (!is.null(pdir)) {
       plot_syn_cons(out, hexcolors, # calls the plot_syn_cons function to produce a plot for the participant in the specified "pdir" directory.
@@ -74,9 +84,15 @@ consistency_scoring <- function(pfp, pdir=NULL, method = "euclidean", fmt = "Luv
 
   }
   df_total <- cbind(dat[, 5], dat[, 3], df_total)
-  colnames(df_total) <- c("PROFILEID", "Cons_test_time", LETTERS, "Å", "Ä", # sets column names
-                          "Ö", 0:9, "Participant_mean", "Part_mean_A_Z",
-                          "Part_mean_0_9", "three_responses", "Prop_black",
-                          "Prop_blue", "Prop_hazy", "Prop_white")
+  colnames(df_total) <- c("PROFILEID", "cons_test_time", # sets column names
+                          LETTERS, "Å", "Ä", "Ö", 0:9,
+                          "part_mean", "part_mean_A_Z", "part_mean_0_9",
+                          "three_resp_tot", "three_resp_A_Z", "three_resp_0_9",
+                          "prop_black_tot", "prop_blue_tot",
+                          "prop_hazy_tot", "prop_white_tot",
+                          "prop_black_A_Z", "prop_blue_A_Z",
+                          "prop_hazy_A_Z", "prop_white_A_Z",
+                          "prop_black_0_9", "prop_blue_0_9",
+                          "prop_hazy_0_9", "prop_white_0_9")
   return(df_total)
 }
